a5d8bb5323798d2b61296e7dd0f47aa3ac2f9149
Mostafa-Ghazi <32398665+Mostafa-Ghazi@users.noreply.github.com>
2021-04-09 09:26:44 +0200

    Update netPerformance.m

--- carrnn/netPerformance.m
+++ carrnn/netPerformance.m
@@ -12,7 +12,7 @@ function performance = netPerformance(y, s, metric)
 %%%%%  This code is an implementation of the algorithm published in:  %%%%%
 %%%%%  CARRNN: A Continuous Autoregressive Recurrent Neural Network   %%%%%
 %%%%%  for Deep Representation Learning from Sporadic Temporal Data   %%%%%
-%%%%%                https://arxiv.org/abs/                           %%%%%
+%%%%%                https://arxiv.org/abs/2104.03739                 %%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Measuring the network prediction performance
