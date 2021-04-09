e62bdf55d571e08fe8adae53ab61d138e4b913ee
Mostafa-Ghazi <32398665+Mostafa-Ghazi@users.noreply.github.com>
2021-04-09 09:24:22 +0200

    Update activationFunction.m

--- carrnn/activationFunction.m
+++ carrnn/activationFunction.m
@@ -12,7 +12,7 @@ function f = activationFunction(x, type)
 %%%%%  This code is an implementation of the algorithm published in:  %%%%%
 %%%%%  CARRNN: A Continuous Autoregressive Recurrent Neural Network   %%%%%
 %%%%%  for Deep Representation Learning from Sporadic Temporal Data   %%%%%
-%%%%%                https://arxiv.org/abs/                           %%%%%
+%%%%%                https://arxiv.org/abs/2104.03739                 %%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Network layer activation function
