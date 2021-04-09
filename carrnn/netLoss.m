7bd29b9de6ac9fbf0029e3d9074dd6c678aba8c1
Mostafa-Ghazi <32398665+Mostafa-Ghazi@users.noreply.github.com>
2021-04-09 09:26:26 +0200

    Update netLoss.m

--- carrnn/netLoss.m
+++ carrnn/netLoss.m
@@ -12,7 +12,7 @@ function [loss, NetParam] = netLoss(NetParam, y)
 %%%%%  This code is an implementation of the algorithm published in:  %%%%%
 %%%%%  CARRNN: A Continuous Autoregressive Recurrent Neural Network   %%%%%
 %%%%%  for Deep Representation Learning from Sporadic Temporal Data   %%%%%
-%%%%%                https://arxiv.org/abs/                           %%%%%
+%%%%%                https://arxiv.org/abs/2104.03739                 %%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Calculating the network loss and output gradients
