0f407edb0c46a14249f73091f1c1b2561d2a1f8b
Mostafa-Ghazi <32398665+Mostafa-Ghazi@users.noreply.github.com>
2021-04-09 09:27:26 +0200

    Update netThreshold.m

--- carrnn/netThreshold.m
+++ carrnn/netThreshold.m
@@ -12,7 +12,7 @@ function NetParam = netThreshold(NetParam)
 %%%%%  This code is an implementation of the algorithm published in:  %%%%%
 %%%%%  CARRNN: A Continuous Autoregressive Recurrent Neural Network   %%%%%
 %%%%%  for Deep Representation Learning from Sporadic Temporal Data   %%%%%
-%%%%%                https://arxiv.org/abs/                           %%%%%
+%%%%%                https://arxiv.org/abs/2104.03739                 %%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Thresholding the network gradients
