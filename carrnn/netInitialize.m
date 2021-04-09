4458018154ee75ae7dd4eed0bd47b87c10ec53df
Mostafa-Ghazi <32398665+Mostafa-Ghazi@users.noreply.github.com>
2021-04-09 09:26:07 +0200

    Update netInitialize.m

--- carrnn/netInitialize.m
+++ carrnn/netInitialize.m
@@ -12,7 +12,7 @@ function NetParam = netInitialize(NetParam, variableType)
 %%%%%  This code is an implementation of the algorithm published in:  %%%%%
 %%%%%  CARRNN: A Continuous Autoregressive Recurrent Neural Network   %%%%%
 %%%%%  for Deep Representation Learning from Sporadic Temporal Data   %%%%%
-%%%%%                https://arxiv.org/abs/                           %%%%%
+%%%%%                https://arxiv.org/abs/2104.03739                 %%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Initialization of the network parameters
